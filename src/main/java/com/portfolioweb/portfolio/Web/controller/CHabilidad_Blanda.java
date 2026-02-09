41ab14a5a22e523ad6627b7cf6a3f06afff745a8
webMelinda <110143889+webMelinda@users.noreply.github.com>
2026-02-09 12:37:19 -0300

    Bind server port from PORT env

--- src/main/java/com/portfolioweb/portfolio/Web/controller/CHabilidad_Blanda.java
+++ src/main/java/com/portfolioweb/portfolio/Web/controller/CHabilidad_Blanda.java
@@ -5,7 +5,6 @@ import com.portfolioweb.portfolio.Web.entity.Habilidad_Blanda;
 import com.portfolioweb.portfolio.Web.service.SHabilidad_Blanda;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("soft") 
-@CrossOrigin(origins = "https://portfolio-frontend-melinda.web.app")
 public class CHabilidad_Blanda {
      @Autowired
     SHabilidad_Blanda blandServ;
--- src/main/java/com/portfolioweb/portfolio/Web/controller/CRed_Social.java
+++ src/main/java/com/portfolioweb/portfolio/Web/controller/CRed_Social.java
@@ -5,7 +5,6 @@ import com.portfolioweb.portfolio.Web.entity.Red_Social;
 import com.portfolioweb.portfolio.Web.service.SRed_Social;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("red")
-@CrossOrigin(origins = "https://portfolio-frontend-melinda.web.app")
 public class CRed_Social {
     @Autowired
     SRed_Social redsocServ;
